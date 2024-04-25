005b260362944082b6b42adad878eab354c14ed8
albert <angelalberto545@gmail.com>
2024-04-25 15:35:35 -0400

    Initial Commit

---
+++ appAaron/appAaronApp.swift
@@ -0,0 +1,17 @@
+//
+//  appAaronApp.swift
+//  appAaron
+//
+//  Created by Angel alberto Bernechea on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct appAaronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
